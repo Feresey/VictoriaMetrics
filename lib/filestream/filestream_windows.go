5cbf755a7e165dfef77958be3655f31dcac02a2e
Feresey <p.milko1999@yandex.ru>
2019-12-21 21:49:30 +0300

    use all struct fields

--- lib/filestream/filestream_windows.go
+++ lib/filestream/filestream_windows.go
@@ -1,5 +1,8 @@
 package filestream
 
+// all fields must be used
+var _ = streamTracker{0, 0, 0}
+
 func (st *streamTracker) adviseDontNeed(n int, fdatasync bool) error {
 	return nil
 }
--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 case $1 in
 "windows")
 	export CGO_ENABLED=1
-	export GOARCH=amd64
+	export GOARCH=386
 	export GOOS=windows
 	export GO111MODULE=on
 	export CC=x86_64-w64-mingw32-gcc
 	export CXX=x86_64-w64-mingw32-g++
+    export CGO_CFLAGS="-O2 -m32"
 	go build -v -mod=vendor -ldflags "-X 'github.com/VictoriaMetrics/VictoriaMetrics/lib/buildinfo.Version=victoria-metrics-20191115-104921-heads-master-0-gc56b9ed-dirty-5f10b01e'" -ldflags '-w -s -extldflags "-static"' -o bin/victoria-metrics-win.exe github.com/VictoriaMetrics/VictoriaMetrics/app/victoria-metrics\
         && wine ./bin/victoria-metrics-win.exe
 	;;
